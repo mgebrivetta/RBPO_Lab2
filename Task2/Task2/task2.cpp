814f1183740541ecf52a2b33cad5f55fdf36ead3
mgebrivetta <90390526+mgebrivetta@users.noreply.github.com>
2022-01-14 17:12:12 +0300

    Add files via upload

---
+++ Task2/Task2/task2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+
+double f(double x) {
+    double f;
+    f = (cos(x) + sin(x)) / (cos(x) - sin(x));
+    return f;
+}
+
+int main() {
+    double x = 5;
+    std::cout << "x = " << x << std::endl;
+    std::cout << "f = " << f(x) << std::endl;
+    std::cout << "x = ";
+    std::cin >> x;
+    std::cout << "f = " << f(x) << std::endl;
+    return 0;
+}
