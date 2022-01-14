4ac125869eaf978adf253293fe2be50dcdd9538b
mgebrivetta <90390526+mgebrivetta@users.noreply.github.com>
2022-01-14 17:14:13 +0300

    Add files via upload

---
+++ task5/task5/task5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+void f();
+double res;
+double x = 5;
+
+int main() {
+    std::cout << "x = " << x << std::endl;
+    f();
+    std::cout << "f = " << res << std::endl;
+    std::cout << "x = ";
+    std::cin >> x;
+    f();
+    std::cout << "f = " << res << std::endl;
+    return 0;
+}
+
+void f() {
+    res = (cos(x) + sin(x)) / (cos(x) - sin(x));
+}
+
+
+
