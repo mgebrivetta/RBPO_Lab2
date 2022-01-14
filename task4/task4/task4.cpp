f3f46a0bd5f275d591582610199cfc308cde7c3e
mgebrivetta <90390526+mgebrivetta@users.noreply.github.com>
2022-01-14 17:13:43 +0300

    Add files via upload

---
+++ task4/task4/task4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+void f(const double& x, double& res);
+
+int main() {
+    double x = 5, y;
+    f(x, y);
+    std::cout << "x = " << x << std::endl;
+    std::cout << "f = " << y << std::endl;
+    std::cout << "x = ";
+    std::cin >> x;
+    f(x, y);
+    std::cout << "f = " << y << std::endl;
+    return 0;
+}
+
+void f(const double& x, double& res) {
+    res = (cos(x) + sin(x)) / (cos(x) - sin(x));
+}
+
